b3aac66fa14dea16bfb1e95fb77b07ae39dc31cc
Filip Gospodinov <filip@monetas.net>
2014-08-14 10:55:58 +0200

    cppcheck: include headers for better checking
    In a first step the headers are included for checking opentxs-api.
    Eventually, the include arguments will be integrated into
    cppcheck_wrapper.sh which will be called only once as it was before.

--- scripts/travis/cppcheck_wrapper.sh
+++ scripts/travis/cppcheck_wrapper.sh
@@ -7,6 +7,8 @@ sup_info="--suppress=ConfigurationNotChecked"
 suppress="$sup_warn $sup_info"
 enabled="--enable=warning --enable=information --enable=performance \
          --enable=portability --enable=missingInclude --enable=style"
+# Travis kills the check if he does not see output for some time
+( while true; do sleep 300; echo "cppcheck in progress ..."; done) &
 # Exit code '1' is returned if arguments are not valid or if no input
 # files are provided. Compare 'cppcheck --help'.
 cppcheck -f -q --inline-suppr --error-exitcode=2 $enabled $suppress "$@"
