6dd6d1841dd18b6cd129b47e0ef108e9954be771
Filip Gospodinov <filip@monetas.net>
2014-09-19 09:09:34 +0200

    OTSmartContract: remove {Load,Save}Template()

--- include/opentxs/core/script/OTSmartContract.hpp
+++ include/opentxs/core/script/OTSmartContract.hpp
@@ -474,20 +474,6 @@ public:
     EXPORT bool LoadEditable(const OTString& strName);
     EXPORT bool SaveEditable(const OTString& strName);
 
-    // Once a smart contract is issued onto a server, then it's available there
-    // as a template.
-    // Users can download a list of templates, and create an instance of a smart
-    // contract. From
-    // there they can confirm and activate it along with the other parties.
-    // (You have have multiple instances of a single template. For example, two
-    // escrow contracts
-    // might be otherwise identical, except for gold in one instance, and silver
-    // in another.)
-    //
-    EXPORT bool LoadTemplate(const OTIdentifier& SERVER_ID,
-                             const OTIdentifier& CONTRACT_ID);
-    EXPORT bool SaveTemplate(const OTIdentifier& SERVER_ID);
-
     EXPORT virtual ~OTSmartContract();
 
     void InitSmartContract();
