142857779a2782bbf13725020e881424eb539cc9
Otto Allmendinger <otto.allmendinger@gmail.com>
2014-09-28 16:33:20 +0200

    Use print_stacktrace() instead of inline code
    The `print_stacktrace()` function defined in `core/util/stacktrace.h`
    essentially does the same thing and also does the platform-specific stuff. It
    should be used in a few other places in this file as well.
    
    This still prints stacktrace twice in OT_FAIL* and OT_ASSERT* macros (and three
    times if we enable signal handlers).
    
    The `print_stacktrace()` call from OTLog::logAssert could be skipped if we know
    for sure that `ot_terminate()` is our handler for `std::terminate()`. Maybe
    introduce a flag `OT_ASSERT_PRINT_STACKTRACE` to control that.

--- src/core/OTLog.cpp
+++ src/core/OTLog.cpp
@@ -959,69 +959,7 @@ void ot_terminate()
         }
     }
 
-#if !defined(_WIN32) && !defined(ANDROID)
-//  Print a stack trace to stderr.
-//  FIXME: this code is repeated multiple times in this file and should
-//         be put in a separate method.
-
-    void* array[50];
-    int32_t size = backtrace(array, 50);
-
-    char** messages = backtrace_symbols(array, size);
-
-    // skip first stack frame (points here)
-    for (int32_t i = 1; i < size && messages != nullptr; ++i) {
-        char* mangled_name = 0, *offset_begin = 0, *offset_end = 0;
-
-        // find parantheses and +address offset surrounding mangled name
-        for (char* p = messages[i]; *p; ++p) {
-            if (*p == '(') {
-                mangled_name = p;
-            }
-            else if (*p == '+') {
-                offset_begin = p;
-            }
-            else if (*p == ')') {
-                offset_end = p;
-                break;
-            }
-        }
-
-        // if the line could be processed, attempt to demangle the symbol
-        if (mangled_name && offset_begin && offset_end &&
-            mangled_name < offset_begin) {
-            *mangled_name++ = '\0';
-            *offset_begin++ = '\0';
-            *offset_end++ = '\0';
-
-            int32_t status;
-            char* real_name = abi::__cxa_demangle(mangled_name, 0, 0, &status);
-
-            // if demangling is successful, output the demangled function name
-            if (status == 0) {
-                std::cerr << "[bt]: (" << i << ") " << messages[i] << " : "
-                          << real_name << "+" << offset_begin << offset_end
-                          << std::endl;
-
-            }
-            // otherwise, output the mangled function name
-            else {
-                std::cerr << "[bt]: (" << i << ") " << messages[i] << " : "
-                          << mangled_name << "+" << offset_begin << offset_end
-                          << std::endl;
-            }
-            if (nullptr != real_name) free(real_name);
-        }
-        // otherwise, print the whole line
-        else {
-            std::cerr << "[bt]: (" << i << ") " << messages[i] << std::endl;
-        }
-    }
-    std::cerr << std::endl;
-
-    free(messages);
-
-#endif
+    print_stacktrace();
 
     // Call the default std::terminate() handler.
     std::abort();
