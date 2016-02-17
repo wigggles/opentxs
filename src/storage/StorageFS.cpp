aa9165c36f09ac18b2c2de0e351c07ef8692b3a0
Justus Ranvier <justus@stashcrypto.com>
2016-02-17 11:45:19 -0600

    correct return types

--- src/storage/StorageFS.cpp
+++ src/storage/StorageFS.cpp
@@ -79,7 +79,7 @@ std::string StorageFS::LoadRoot()
     if (!folder_.empty()) {
         std::string filename = folder_ + "/" + config_.fs_root_file_;
 
-        if (!boost::filesystem::exists(filename)) { return false; }
+        if (!boost::filesystem::exists(filename)) { return ""; }
 
         std::ifstream file(
             filename,
@@ -88,7 +88,7 @@ std::string StorageFS::LoadRoot()
         if (file.good()) {
             std::ifstream::pos_type size = file.tellg();
 
-            if (0 >= size) { return false; }
+            if (0 >= size) { return ""; }
 
             file.seekg(0, std::ios::beg);
 
