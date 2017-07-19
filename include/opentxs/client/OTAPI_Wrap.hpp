1ea99b78cfc2a1ba577ee8bfe120688132a1a43b
Justus Ranvier <justus@stashcrypto.com>
2017-07-19 13:53:32 -0500

    OTAPI_Wrap::AppInit: allow archive directory to be passed

--- include/opentxs/client/OTAPI_Wrap.hpp
+++ include/opentxs/client/OTAPI_Wrap.hpp
@@ -75,7 +75,9 @@ public:
 
      Call this once per run of the application.
      */
-    EXPORT static bool AppInit(const std::string& storagePlugin = "");
+    EXPORT static bool AppInit(
+        const std::string& storagePlugin = "",
+        const std::string& archiveDirectory = "");
     EXPORT static bool AppCleanup();  // Call this ONLY ONCE, when your App is
                                       // shutting down.
 
