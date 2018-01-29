52e1d6d8c5a0b66bc04c6b2b3f680c5db864f627
Justus Ranvier <justus@stashcrypto.com>
2018-01-29 12:46:39 -0600

    opentxs::api::client::ServerAction: construct Actions

--- include/opentxs/api/client/ServerAction.hpp
+++ include/opentxs/api/client/ServerAction.hpp
@@ -41,8 +41,31 @@
 
 #include "opentxs/Version.hpp"
 
+#include "opentxs/Pimpl.hpp"
+#include "opentxs/Proto.hpp"
+#include "opentxs/Types.hpp"
+
+#include <chrono>
+#include <string>
+
 namespace opentxs
 {
+class Cheque;
+class Identifier;
+class Ledger;
+class Nym;
+class OTPayment;
+class OTPaymentPlan;
+class OTSmartContract;
+class Purse;
+class ServerContract;
+class String;
+class UnitDefinition;
+
+namespace client
+{
+class ServerAction;
+}  // namespace client
 namespace api
 {
 namespace client
@@ -51,7 +74,280 @@ namespace client
 class ServerAction
 {
 public:
-    virtual ~ServerAction() = default;
+    using Action = Pimpl<opentxs::client::ServerAction>;
+
+    EXPORT virtual Action AcknowledgeBailment(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& recipientID,
+        const Identifier& requestID,
+        const std::string& instructions) const = 0;
+    EXPORT virtual Action AcknowledgeConnection(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& recipientID,
+        const Identifier& requestID,
+        const bool ack,
+        const std::string& url,
+        const std::string& login,
+        const std::string& password,
+        const std::string& key) const = 0;
+    EXPORT virtual Action AcknowledgeNotice(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& recipientID,
+        const Identifier& requestID,
+        const bool ack) const = 0;
+    EXPORT virtual Action AcknowledgeOutbailment(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& recipientID,
+        const Identifier& requestID,
+        const std::string& details) const = 0;
+    EXPORT virtual Action ActivateSmartContract(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const String& agentName,
+        const OTSmartContract& contract) const = 0;
+    EXPORT virtual Action AddServerClaim(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const proto::ContactSectionName section,
+        const proto::ContactItemType type,
+        const std::string& value,
+        bool primary) const = 0;
+    EXPORT virtual Action AdjustUsageCredits(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetNymID,
+        const Amount adjustment) const = 0;
+    EXPORT virtual Action CancelPaymentPlan(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const OTPaymentPlan& plan) const = 0;
+    EXPORT virtual Action CreateMarketOffer(
+        const Identifier& assetAccountID,
+        const Identifier& currencyAccountID,
+        const Amount scale,
+        const Amount increment,
+        const Amount quantity,
+        const Amount price,
+        const bool selling,
+        const std::chrono::seconds lifetime,
+        const std::string& stopSign,
+        const Amount activationPrice) const = 0;
+#if OT_CASH
+    EXPORT virtual Action DepositCashPurse(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const Purse& purse) const = 0;
+#endif  // OT_CASH
+    EXPORT virtual Action DepositCheque(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const Cheque& cheque) const = 0;
+    EXPORT virtual Action DepositPaymentPlan(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const OTPaymentPlan& plan) const = 0;
+    EXPORT virtual bool DownloadAccount(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID) const = 0;
+    EXPORT virtual Action DownloadBoxReceipt(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const RemoteBoxType box,
+        const TransactionNumber item) const = 0;
+    EXPORT virtual Action DownloadContract(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& contractID) const = 0;
+    EXPORT virtual Action DownloadMarketList(
+        const Identifier& localNymID,
+        const Identifier& serverID) const = 0;
+    EXPORT virtual Action DownloadMarketOffers(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& marketID,
+        const Amount depth) const = 0;
+    EXPORT virtual Action DownloadMarketRecentTrades(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& marketID) const = 0;
+#if OT_CASH
+    EXPORT virtual Action DownloadMint(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& instrumentDefinitionID) const = 0;
+#endif  // OT_CASH
+    EXPORT virtual Action DownloadNym(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetNymID) const = 0;
+    EXPORT virtual Action DownloadNymMarketOffers(
+        const Identifier& localNymID,
+        const Identifier& serverID) const = 0;
+    EXPORT virtual Action ExchangeBasketCurrency(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& instrumentDefinitionID,
+        const Identifier& accountID,
+        const Identifier& basketID,
+        const bool direction) const = 0;
+#if OT_CASH
+    EXPORT virtual Action ExchangeCash(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& instrumentDefinitionID,
+        const Purse& purse) const = 0;
+#endif  // OT_CASH
+    EXPORT virtual bool GetTransactionNumbers(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const std::size_t quantity) const = 0;
+    EXPORT virtual Action InitiateBailment(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetNymID,
+        const Identifier& instrumentDefinitionID) const = 0;
+    EXPORT virtual Action InitiateOutbailment(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetNymID,
+        const Identifier& instrumentDefinitionID,
+        const Amount amount,
+        const std::string& message) const = 0;
+    EXPORT virtual Action InitiateRequestConnection(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetNymID,
+        const proto::ConnectionInfoType& type) const = 0;
+    EXPORT virtual Action InitiateStoreSecret(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetNymID,
+        const proto::SecretType& type,
+        const std::string& primary,
+        const std::string& secondary) const = 0;
+    EXPORT virtual Action IssueBasketCurrency(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const proto::UnitDefinition& basket) const = 0;
+    EXPORT virtual Action IssueUnitDefinition(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const proto::UnitDefinition& contract) const = 0;
+    EXPORT virtual Action KillMarketOffer(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const TransactionNumber number) const = 0;
+    EXPORT virtual Action KillPaymentPlan(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const TransactionNumber number) const = 0;
+    EXPORT virtual Action NotifyBailment(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetNymID,
+        const Identifier& instrumentDefinitionID,
+        const Identifier& requestID,
+        const std::string& txid) const = 0;
+    EXPORT virtual Action PayDividend(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& instrumentDefinitionID,
+        const Identifier& accountID,
+        const std::string& memo,
+        const Amount amountPerShare) const = 0;
+    EXPORT virtual Action ProcessInbox(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const Ledger& ledger) const = 0;
+    EXPORT virtual Action PublishNym(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetNymID) const = 0;
+    EXPORT virtual Action PublishServerContract(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetServerID) const = 0;
+    EXPORT virtual Action PublishUnitDefinition(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& unitDefinitionID) const = 0;
+    EXPORT virtual Action RegisterAccount(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& instrumentDefinitionID) const = 0;
+    EXPORT virtual Action RegisterNym(
+        const Identifier& localNymID,
+        const Identifier& serverID) const = 0;
+    EXPORT virtual Action RequestAdmin(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const std::string& password) const = 0;
+#if OT_CASH
+    EXPORT virtual Action SendCash(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& recipientNymID,
+        const Purse& recipientCopy,
+        const Purse& senderCopy) const = 0;
+#endif  // OT_CASH
+    EXPORT virtual Action SendMessage(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& recipientNymID,
+        const std::string& message) const = 0;
+    EXPORT virtual Action SendPayment(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& recipientNymID,
+        const OTPayment& payment) const = 0;
+    EXPORT virtual Action SendTransfer(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& senderAccountID,
+        const Identifier& recipientAccountID,
+        const Amount amount,
+        const std::string& memo) const = 0;
+    EXPORT virtual Action TriggerClause(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const TransactionNumber transactionNumber,
+        const std::string& clause,
+        const std::string& parameter) const = 0;
+    EXPORT virtual Action UnregisterAccount(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID) const = 0;
+    EXPORT virtual Action UnregisterNym(
+        const Identifier& localNymID,
+        const Identifier& serverID) const = 0;
+#if OT_CASH
+    EXPORT virtual Action WithdrawCash(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const Amount amount) const = 0;
+#endif  // OT_CASH
+    EXPORT virtual Action WithdrawVoucher(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const Identifier& recipientNymID,
+        const Amount amount,
+        const std::string& memo) const = 0;
+
+    EXPORT virtual ~ServerAction() = default;
 
 protected:
     ServerAction() = default;
--- include/opentxs/api/client/implementation/ServerAction.hpp
+++ include/opentxs/api/client/implementation/ServerAction.hpp
@@ -45,6 +45,9 @@
 
 namespace opentxs
 {
+class OT_API;
+class OTAPI_Exec;
+
 namespace api
 {
 namespace implementation
@@ -53,18 +56,300 @@ class Api;
 }  // namespace implementation
 namespace client
 {
+class Wallet;
+
 namespace implementation
 {
 
 class ServerAction : virtual public opentxs::api::client::ServerAction
 {
 public:
+    Action AcknowledgeBailment(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& recipientID,
+        const Identifier& requestID,
+        const std::string& instructions) const override;
+    Action AcknowledgeConnection(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& recipientID,
+        const Identifier& requestID,
+        const bool ack,
+        const std::string& url,
+        const std::string& login,
+        const std::string& password,
+        const std::string& key) const override;
+    Action AcknowledgeNotice(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& recipientID,
+        const Identifier& requestID,
+        const bool ack) const override;
+    Action AcknowledgeOutbailment(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& recipientID,
+        const Identifier& requestID,
+        const std::string& details) const override;
+    Action ActivateSmartContract(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const String& agentName,
+        const OTSmartContract& contract) const override;
+    Action AddServerClaim(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const proto::ContactSectionName section,
+        const proto::ContactItemType type,
+        const std::string& value,
+        bool primary) const override;
+    Action AdjustUsageCredits(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetNymID,
+        const Amount adjustment) const override;
+    Action CancelPaymentPlan(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const OTPaymentPlan& plan) const override;
+    Action CreateMarketOffer(
+        const Identifier& assetAccountID,
+        const Identifier& currencyAccountID,
+        const Amount scale,
+        const Amount increment,
+        const Amount quantity,
+        const Amount price,
+        const bool selling,
+        const std::chrono::seconds lifetime,
+        const std::string& stopSign,
+        const Amount activationPrice) const override;
+#if OT_CASH
+    Action DepositCashPurse(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const Purse& purse) const override;
+#endif  // OT_CASH
+    Action DepositCheque(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const Cheque& cheque) const override;
+    Action DepositPaymentPlan(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const OTPaymentPlan& plan) const override;
+    bool DownloadAccount(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID) const override;
+    Action DownloadBoxReceipt(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const RemoteBoxType box,
+        const TransactionNumber item) const override;
+    Action DownloadContract(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& contractID) const override;
+    Action DownloadMarketList(
+        const Identifier& localNymID,
+        const Identifier& serverID) const override;
+    Action DownloadMarketOffers(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& marketID,
+        const Amount depth) const override;
+    Action DownloadMarketRecentTrades(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& marketID) const override;
+    Action DownloadNymMarketOffers(
+        const Identifier& localNymID,
+        const Identifier& serverID) const override;
+#if OT_CASH
+    Action DownloadMint(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& instrumentDefinitionID) const override;
+#endif  // OT_CASH
+    Action DownloadNym(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetNymID) const override;
+    Action ExchangeBasketCurrency(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& instrumentDefinitionID,
+        const Identifier& accountID,
+        const Identifier& basketID,
+        const bool direction) const override;
+#if OT_CASH
+    Action ExchangeCash(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& instrumentDefinitionID,
+        const Purse& purse) const override;
+#endif  // OT_CASH
+    bool GetTransactionNumbers(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const std::size_t quantity) const override;
+    Action InitiateBailment(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetNymID,
+        const Identifier& instrumentDefinitionID) const override;
+    Action InitiateOutbailment(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetNymID,
+        const Identifier& instrumentDefinitionID,
+        const Amount amount,
+        const std::string& message) const override;
+    Action InitiateRequestConnection(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetNymID,
+        const proto::ConnectionInfoType& type) const override;
+    Action InitiateStoreSecret(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetNymID,
+        const proto::SecretType& type,
+        const std::string& primary,
+        const std::string& secondary) const override;
+    Action IssueBasketCurrency(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const proto::UnitDefinition& basket) const override;
+    Action IssueUnitDefinition(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const proto::UnitDefinition& contract) const override;
+    Action KillMarketOffer(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const TransactionNumber number) const override;
+    Action KillPaymentPlan(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const TransactionNumber number) const override;
+    Action NotifyBailment(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetNymID,
+        const Identifier& instrumentDefinitionID,
+        const Identifier& requestID,
+        const std::string& txid) const override;
+    Action PayDividend(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& instrumentDefinitionID,
+        const Identifier& accountID,
+        const std::string& memo,
+        const Amount amountPerShare) const override;
+    Action ProcessInbox(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const Ledger& ledger) const override;
+    Action PublishNym(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetNymID) const override;
+    Action PublishServerContract(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& targetServerID) const override;
+    Action PublishUnitDefinition(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& unitDefinitionID) const override;
+    Action RegisterAccount(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& instrumentDefinitionID) const override;
+    Action RegisterNym(const Identifier& localNymID, const Identifier& serverID)
+        const override;
+    Action RequestAdmin(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const std::string& password) const override;
+#if OT_CASH
+    Action SendCash(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& recipientNymID,
+        const Purse& recipientCopy,
+        const Purse& senderCopy) const override;
+#endif  // OT_CASH
+    Action SendMessage(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& recipientNymID,
+        const std::string& message) const override;
+    Action SendPayment(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& recipientNymID,
+        const OTPayment& payment) const override;
+    Action SendTransfer(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& senderAccountID,
+        const Identifier& recipientAccountID,
+        const Amount amount,
+        const std::string& memo) const override;
+    Action TriggerClause(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const TransactionNumber transactionNumber,
+        const std::string& clause,
+        const std::string& parameter) const override;
+    Action UnregisterAccount(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID) const override;
+    Action UnregisterNym(
+        const Identifier& localNymID,
+        const Identifier& serverID) const override;
+#if OT_CASH
+    Action WithdrawCash(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const Amount amount) const override;
+#endif  // OT_CASH
+    Action WithdrawVoucher(
+        const Identifier& localNymID,
+        const Identifier& serverID,
+        const Identifier& accountID,
+        const Identifier& recipientNymID,
+        const Amount amount,
+        const std::string& memo) const override;
+
     ~ServerAction() = default;
 
 private:
     friend api::implementation::Api;
 
-    ServerAction() = default;
+    std::recursive_mutex& api_lock_;
+    const OT_API& otapi_;
+    const OTAPI_Exec& exec_;
+    const api::client::Wallet& wallet_;
+
+    ServerAction(
+        std::recursive_mutex& apiLock,
+        const OT_API& otapi,
+        const OTAPI_Exec& exec,
+        const api::client::Wallet& wallet);
+    ServerAction() = delete;
     ServerAction(const ServerAction&) = delete;
     ServerAction(ServerAction&&) = delete;
     ServerAction& operator=(const ServerAction&) = delete;
