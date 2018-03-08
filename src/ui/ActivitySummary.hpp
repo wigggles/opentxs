a20c77b87e32b27958b4d9f46a3cb898c23dc78c
Justus Ranvier <justus@stashcrypto.com>
2018-03-08 12:39:08 -0600

    ui::implementation:List
    Collect common member variables and functions into a common
    template

--- src/ui/ActivitySummary.hpp
+++ src/ui/ActivitySummary.hpp
@@ -47,74 +47,55 @@
 #include "opentxs/ui/ActivitySummary.hpp"
 #include "opentxs/ui/ActivitySummaryItem.hpp"
 
+#include "List.hpp"
+
 #include <map>
 #include <string>
-#include <thread>
 #include <tuple>
 
 namespace opentxs::ui::implementation
 {
-class ActivitySummary : virtual public ui::ActivitySummary, Lockable
+using ActivitySummaryPimpl = OTUIActivitySummaryItem;
+using ActivitySummaryID = Identifier;
+using ActivitySummarySortKey =
+    std::pair<std::chrono::system_clock::time_point, std::string>;
+using ActivitySummaryInner = std::map<ActivitySummaryID, ActivitySummaryPimpl>;
+using ActivitySummaryOuter =
+    std::map<ActivitySummarySortKey, ActivitySummaryInner>;
+using ActivitySummaryReverse =
+    std::map<ActivitySummaryID, ActivitySummarySortKey>;
+using ActivitySummaryType = List<
+    opentxs::ui::ActivitySummary,
+    opentxs::ui::ActivitySummaryItem,
+    ActivitySummaryID,
+    ActivitySummaryPimpl,
+    ActivitySummaryInner,
+    ActivitySummarySortKey,
+    ActivitySummaryOuter,
+    ActivitySummaryOuter::const_reverse_iterator,
+    ActivitySummaryReverse>;
+
+class ActivitySummary : virtual public ActivitySummaryType
 {
 public:
-    const opentxs::ui::ActivitySummaryItem& First() const override;
-    const opentxs::ui::ActivitySummaryItem& Next() const override;
-
-    ~ActivitySummary();
+    ~ActivitySummary() = default;
 
 private:
     friend api::implementation::UI;
     friend ActivitySummaryItem;
-    /** Thread ID, Thread */
-    using Item = std::map<Identifier, OTUIActivitySummaryItem>;
-    /** Last Activity, Display name */
-    using ItemIndex =
-        std::pair<std::chrono::system_clock::time_point, std::string>;
-    using ItemMap = std::map<ItemIndex, Item>;
-    /** ThreadID, display name*/
-    using NameMap = std::map<Identifier, ItemIndex>;
 
-    const network::zeromq::Context& zmq_;
     const api::Activity& activity_;
-    const api::ContactManager& contact_manager_;
     const Flag& running_;
-    const Identifier owner_nym_id_;
-    mutable Identifier last_thread_id_;
-    const std::unique_ptr<opentxs::ui::ActivitySummaryItem> blank_p_;
-    const opentxs::ui::ActivitySummaryItem& blank_;
-    mutable OTFlag have_items_;
-    mutable OTFlag start_;
-    mutable OTFlag startup_complete_;
-    mutable ItemMap items_;
-    mutable NameMap names_;
-    mutable ItemMap::const_reverse_iterator outer_;
-    mutable Item::const_iterator inner_;
-    std::unique_ptr<std::thread> startup_{nullptr};
     OTZMQListenCallback activity_subscriber_callback_;
     OTZMQSubscribeSocket activity_subscriber_;
 
-    const opentxs::ui::ActivitySummaryItem& current(const Lock& lock) const;
-    const opentxs::ui::ActivitySummaryItem& first(const Lock& lock) const;
-    bool first_valid_item(const Lock& lock) const;
-    void increment_inner(const Lock& lock) const;
-    bool increment_outer(const Lock& lock) const;
-    // Only used by ActivitySummaryItem
-    bool last(const Identifier& id) const;
-    const opentxs::ui::ActivitySummaryItem& next(const Lock& lock) const;
-    // Only used by ActivitySummaryItem
-    void reindex_item(const Identifier& threadID, const ItemIndex& newIndex)
-        const;
-    void reindex_item(
-        const Lock& lock,
-        const Identifier& threadID,
-        const ItemIndex& oldIndex,
-        const ItemIndex& newIndex) const;
+    void construct_item(
+        const ActivitySummaryID& id,
+        const ActivitySummarySortKey& index) const override;
+    ActivitySummaryOuter::const_reverse_iterator outer_first() const override;
+    ActivitySummaryOuter::const_reverse_iterator outer_end() const override;
 
-    void add_item(const std::string& threadID);
-    void add_item(
-        const Identifier& threadID,
-        const std::string& name,
-        const std::chrono::system_clock::time_point& time);
+    void process_thread(const std::string& threadID);
     void process_thread(const network::zeromq::Message& message);
     void startup();
 
