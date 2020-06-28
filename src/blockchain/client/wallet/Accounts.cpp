433d8630279d8608e11efce1608e42f52f21425b
Justus Ranvier <justus@opentransactions.org>
2020-06-28 17:05:54 -0500

    Split up Wallet.cpp

---
+++ src/blockchain/client/wallet/Accounts.cpp
@@ -0,0 +1,113 @@
+// Copyright (c) 2010-2019 The Open-Transactions developers
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "0_stdafx.hpp"                  // IWYU pragma: associated
+#include "1_Internal.hpp"                // IWYU pragma: associated
+#include "blockchain/client/Wallet.hpp"  // IWYU pragma: associated
+
+#include <map>
+#include <set>
+
+#include "internal/api/client/Client.hpp"
+#include "internal/blockchain/Blockchain.hpp"
+#include "opentxs/Pimpl.hpp"
+#include "opentxs/api/Factory.hpp"
+#include "opentxs/api/Wallet.hpp"
+#include "opentxs/api/client/Manager.hpp"
+#include "opentxs/core/Log.hpp"
+#include "opentxs/core/LogSource.hpp"
+#include "opentxs/core/identifier/Nym.hpp"
+#include "opentxs/network/zeromq/Frame.hpp"
+
+// #define OT_METHOD
+// "opentxs::blockchain::client::implementation::Wallet::Accounts::"
+
+namespace opentxs::blockchain::client::implementation
+{
+Wallet::Accounts::Accounts(
+    const api::client::Manager& api,
+    const api::client::internal::Blockchain& blockchain,
+    const internal::Network& network,
+    const internal::WalletDatabase& db,
+    const zmq::socket::Push& socket,
+    const Type chain) noexcept
+    : api_(api)
+    , blockchain_api_(blockchain)
+    , network_(network)
+    , db_(db)
+    , socket_(socket)
+    , chain_(chain)
+    , map_(init(api, blockchain, network, db_, socket_, chain))
+{
+}
+
+auto Wallet::Accounts::Add(const identifier::Nym& nym) noexcept -> bool
+{
+    auto [it, added] = map_.try_emplace(
+        nym,
+        api_,
+        blockchain_api_.BalanceTree(nym, chain_),
+        network_,
+        db_,
+        socket_);
+
+    if (added) {
+        LogNormal("Initializing ")(blockchain::internal::DisplayString(chain_))(
+            " wallet for ")(nym)
+            .Flush();
+    }
+
+    return added;
+}
+
+auto Wallet::Accounts::Add(const zmq::Frame& message) noexcept -> bool
+{
+    auto id = api_.Factory().NymID();
+    id->SetString(message);
+
+    if (0 == id->size()) { return false; }
+
+    return Add(id);
+}
+
+auto Wallet::Accounts::init(
+    const api::client::Manager& api,
+    const api::client::internal::Blockchain& blockchain,
+    const internal::Network& network,
+    const internal::WalletDatabase& db,
+    const zmq::socket::Push& socket,
+    const Type chain) noexcept -> AccountMap
+{
+    auto output = AccountMap{};
+
+    for (const auto& nym : api.Wallet().LocalNyms()) {
+        LogNormal("Initializing ")(blockchain::internal::DisplayString(chain))(
+            " wallet for ")(nym)
+            .Flush();
+        output.try_emplace(
+            nym, api, blockchain.BalanceTree(nym, chain), network, db, socket);
+    }
+
+    return output;
+}
+
+auto Wallet::Accounts::Reorg(const block::Position& parent) noexcept -> bool
+{
+    auto output{false};
+
+    for (auto& [nym, account] : map_) { output |= account.reorg(parent); }
+
+    return output;
+}
+
+auto Wallet::Accounts::state_machine() noexcept -> bool
+{
+    auto output{false};
+
+    for (auto& [nym, account] : map_) { output |= account.state_machine(); }
+
+    return output;
+}
+}  // namespace opentxs::blockchain::client::implementation
